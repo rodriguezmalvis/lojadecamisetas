856b007c64505146f9fdb3161406db7a40ff4cac
rodriguezmalvis <rodriguezmalvis@gmail.com>
2017-06-13 19:13:50 -0300

    Capitulos 7,8
    Capitulos 7,8 finalizado

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,10 +1,12 @@
 class ProdutosController < ApplicationController
 
 	def index
-		
-		@produtos_por_nome = Produto.order(:nome).limit 5
-		@produtos_por_preco = Produto.order(preco: :desc).limit 2
-
+		@produtos_por_nome = Produto.order(:nome)
+		@produtos_por_preco = Produto.order(preco: :desc).limit 4
 	end
 
+	def create
+		valores = params.require(:produto).permit(:nome, :descricao, :quantidade, :preco)
+		Produto.create valores
+	end
 end
